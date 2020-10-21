2b226e673788514ff019e72b1a7516f3c5f1fba2
Gage <hi2gage.pdx@gmail.com>
2020-10-21 09:10:31 -0600

    first commit

---
+++ main.py
@@ -0,0 +1,9 @@
+import water
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    water.auto_water()
+
+
+
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
